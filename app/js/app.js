fed082eb7ddb0a53abed96fd23ec79a76267c633
Кирилл Панченко <mrneverzoom@gmail.com>
2024-12-05 16:52:42 +0300

    add blocks

--- app/js/app.js
+++ app/js/app.js
@@ -27,11 +27,50 @@ document.addEventListener('DOMContentLoaded', () => {
 			trigger: ".fixed-2",
 			start: "top 20px",
 			endTrigger: '.end_transition2',
-			end: 'bottom 385px',
+			end: 'bottom 395px',
 			// markers: true,
 			pin: true,
 			pinSpacing: false,
 			animation: action2,
+			toggleActions: 'play reverse play reverse'
+		});
+
+		var action3 = gsap.to('.fixed-3', {ease: 'none', duration: 0.25 });
+		ScrollTrigger.create({
+			trigger: ".fixed-3",
+			start: "top 20px",
+			endTrigger: '.end_transition3',
+			end: 'bottom 395px',
+			// markers: true,
+			pin: true,
+			pinSpacing: false,
+			animation: action3,
+			toggleActions: 'play reverse play reverse'
+		});
+
+		var action4 = gsap.to('.fixed-4', {ease: 'none', duration: 0.25 });
+		ScrollTrigger.create({
+			trigger: ".fixed-4",
+			start: "top 20px",
+			endTrigger: '.end_transition4',
+			end: 'bottom 395px',
+			// markers: true,
+			pin: true,
+			pinSpacing: false,
+			animation: action4,
+			toggleActions: 'play reverse play reverse'
+		});
+
+		var action5 = gsap.to('.fixed-5', {ease: 'none', duration: 0.25 });
+		ScrollTrigger.create({
+			trigger: ".fixed-5",
+			start: "top 20px",
+			endTrigger: '.end_transition5',
+			end: 'bottom 395px',
+			// markers: true,
+			pin: true,
+			pinSpacing: false,
+			animation: action5,
 			toggleActions: 'play reverse play reverse'
 		});
 	}
